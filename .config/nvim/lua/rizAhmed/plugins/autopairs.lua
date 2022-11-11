c7529a0030d027622bd4d608623371c431e33829
riz Ahmed <rzwn.hmd@gmail.com>
2022-11-11 16:16:33 +0100

    branch clean up

--- .config/nvim/lua/rizAhmed/plugins/autopairs.lua
+++
@@ -1,9 +0,0 @@
-local autopair_setup, autopair = pcall(require, "nvim-autopairs")
-if not autopair_setup then
-    print("this shit doesn't work!")
-    return 
-end
-
-autopair.setup({
-    map_cr = true
-})
--- .config/nvim/lua/rizAhmed/plugins/comment.lua
+++
@@ -1,7 +0,0 @@
-local status_comment, comment = pcall(require, "Comment")
-if not status_comment then
-    print("comment didn't work!")
-    return
-end
-
-comment.setup()
